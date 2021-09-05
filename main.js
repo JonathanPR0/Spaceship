5f282a12086d217c79c16ddab0b9b6de092c5677
Jon-PR0 <jonathan.almeida1793@gmail.com>
2021-09-05 17:52:28 -0300

    Commit do dia 05/09

---
+++ main.js
@@ -0,0 +1,7 @@
+const sectionPrint = document.querySelector("#print");
+const sectionHitch = document.querySelector("#hitch");
+const option = document.querySelector("#option");
+const optionButton = document.querySelector("#optionButton");
+const spaceship = document.querySelector("#spaceship");
+const crew = document.querySelector("#crew");
+const hitchButton = document.querySelector("#hitchButton");
